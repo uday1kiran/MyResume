f8683da073345c23acef4402b8ea2aa71cda4aec
UDay Kiran Reddy <ukreddy@erwin.com>
2020-11-30 15:33:52 +0530

    äge

--- homepage/views.py
+++ homepage/views.py
@@ -8,16 +8,19 @@ from datetime import date
 
 
 def home(request):
+    print("----testing----")
     profile = get_object_or_404(Profile)
     skills = Skill.objects.all()
-    print(profile.dateofbirth)
-    # print("Age: "+calculateAge(profile.dateofbirth))
     skills_1 = get_odds(skills, False)
     skills_2 = get_odds(skills, True)
-    return render(request, 'homepage/home.html', {'title': profile.webtitle, 'username': profile.name, 'profile': profile, 'skills_1': skills_1, 'skills_2': skills_2})
-
+    #testing = type(profile.dateofbirth)
+    #print(testing)
+    #print(calculateAge(profile.dateofbirth))
+    return render(request, 'homepage/home.html', {'title': profile.webtitle, 'username': profile.name,'age':calculateAge(profile.dateofbirth), 'profile': profile, 'skills_1': skills_1, 'skills_2': skills_2})
 
 def calculateAge(birthDate):
+    birthDate = birthDate.date()
+    #print(birthDate)
     today = date.today()
     age = today.year - birthDate.year - \
         ((today.month, today.day) < (birthDate.month, birthDate.day))
